a06ab7a7ec2ffb42451e0ef04b49cb33b9599bb1
Christopher Bertels <bakkdoor@flasht.de>
2011-11-08 22:08:47 -0800

    added context class

---
+++ lib/context.rb
@@ -0,0 +1,42 @@
+module Lisp
+  class Context
+    class SymbolNotFound < StandardError
+      def initialize(symbol)
+        super("Symbol not found: #{symbol}")
+      end
+    end
+
+    def initialize(parent = nil)
+      @parent = parent
+      @symbols = {}
+    end
+
+    def get(symbol)
+      if val = @symbols[symbol]
+        return val
+      end
+
+      if @parent
+        return @parent.get(symbol)
+      end
+
+      raise SymbolNotFound.new(symbol)
+    end
+
+    def has?(symbol)
+      if @symbols[symbol]
+        return true
+      end
+
+      if @parent
+        return @parent.has?(symbol)
+      end
+
+      return false
+    end
+
+    def set(symbol, value)
+      @symbols[symbol] = value
+    end
+  end
+end
