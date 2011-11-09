8e00ef9c0be8709778e3375092a3037c37a22d41
Christopher Bertels <bakkdoor@flasht.de>
2011-11-09 03:22:54 -0800

    Added more methods to kernel/object.lisp

--- kernel/object.lisp
+++ kernel/object.lisp
@@ -3,3 +3,15 @@
 
 (defn inspect (x)
   (. x inspect))
+
+(defn ->string (x)
+  (. x to_s))
+
+(defn ->int (x)
+  (. x to_i))
+
+(defn ->float (x)
+  (. x to_f))
+
+(defn ->symbol (x)
+  (. x to_sym))
