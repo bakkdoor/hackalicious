c3e68e0cb2b17cd35d6cd6ac7e0817b3ff388bf6
Christopher Bertels <bakkdoor@flasht.de>
2011-11-16 23:48:29 -0800

    added -v, -c and -cv options to bin/lisp

--- lib/compiler/code_loader.rb
+++ lib/compiler/code_loader.rb
@@ -1,7 +1,15 @@
 module Lisp
   class CodeLoader
-    def self.load_file(source_file)
-      compiled_file = source_file + "c"
+    def self.load_file(source_file, verbose = false)
+      compiled_file = Compiler.lisp_compiled_name(source_file)
+
+      # optionally compile file if not yet compiled
+      # or source file has been updated since last compilation
+      if !File.exists?(Compiler.lisp_compiled_name(source_file)) ||
+          File.stat(compiled_file).mtime < File.stat(source_file).mtime
+        Compiler.compile_lisp_file(source_file, nil, 1, verbose)
+      end
+
       cl = Rubinius::CodeLoader.new(compiled_file)
       begin
         cm = cl.load_compiled_file(compiled_file, 0)
